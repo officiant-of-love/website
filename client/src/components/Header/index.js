eac166f7d5965ecd62ad0b650cee88599a74d92f
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-21 20:12:05 -0700

    add tailwind

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-        <header>
-            <h1 className="title">Officiant of Love</h1>
+        <header className="">
+            <h1 className="">Officiant of Love</h1>
         </header>
     );
 };
