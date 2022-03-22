3c07e65d9f8d54ea0162f6f451bfa173aa150300
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-21 20:27:07 -0700

    add header basics

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-        <header className="">
-            <h1 className="">Officiant of Love</h1>
+        <header className="bg-black opacity-40">
+            <h1 className="font-semibold text-4xl lg:text-6xl text-white p-3 text-center">Officiant of Love</h1>
         </header>
     );
 };
