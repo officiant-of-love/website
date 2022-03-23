8932ff50c9dfeda13ed878bad15eaa6848bc015c
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-22 20:12:27 -0700

    add alt

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,8 +3,7 @@ import closeIcon from './close.svg'
 
 const Nav = ({ mobileNavOpen, setMobileNavOpen }) => {
     const handleClick = (e) => {
-        const { className } = e.target;
-        console.log(className)
+        console.log(e.target.style)
     };
 
     return (
@@ -16,7 +15,7 @@ const Nav = ({ mobileNavOpen, setMobileNavOpen }) => {
                 <li className='px-1'>|</li>
                 <li className='cursor-pointer' onClick={handleClick}>Funerals</li>
             </ul>
-            <img className='align-middle md:hidden pt-1' src={!mobileNavOpen ? hamburgerMenu : closeIcon} onClick={() => setMobileNavOpen(!mobileNavOpen)}/>
+            <img className='align-middle md:hidden pt-1' src={!mobileNavOpen ? hamburgerMenu : closeIcon} onClick={() => setMobileNavOpen(!mobileNavOpen)} alt="mobile menu"/>
         </nav>
     );
 };
