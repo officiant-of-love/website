34a51b820d8d9e872206a9eb7cd984b509c2d028
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-23 14:57:30 -0700

    add hero title

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,7 +1,7 @@
 const Hero = () => {
     return (
-        <section className={`bg-wedding-hero absolute top-0 w-full h-[400px] bg-cover bg-center -z-10`}>
-            Weddings
+        <section className={`bg-wedding-hero absolute top-0 w-full h-[400px] lg:h-[650px] bg-cover bg-center -z-10 flex justify-center`}>
+            <h1 className="text-center text-white font-bold text-6xl lg:text-9xl my-auto lg:pt-[128px]">Weddings</h1>
         </section>
     );
 };
