31dfc8f2bdb64b9ddbdaed36563819ba568483be
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-24 14:53:22 -0700

    basic services component desktop version

--- client/src/pages/Weddings/index.js
+++ client/src/pages/Weddings/index.js
@@ -1,11 +1,13 @@
-import Hero from '../../components/Hero'
+import Hero from '../../components/Hero';
+import Services from '../../components/Services';
 
 
 const Weddings = () => {
     return(
-        <>
+        <main>
             <Hero />
-        </>
+            <Services />
+        </main>
     );
 };
 
