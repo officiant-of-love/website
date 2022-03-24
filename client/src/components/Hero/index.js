4866b0f6d4c56a7e750a33731380302827af45b4
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-24 13:57:24 -0700

    add css to Hero component because tailwind doesn't have text-shadow

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,7 +1,9 @@
+import './style.css'
+
 const Hero = () => {
     return (
-        <section className={`bg-wedding-hero absolute top-0 w-full h-[400px] lg:h-[650px] bg-cover bg-center -z-10 flex justify-center`}>
-            <h1 className="text-center text-white font-bold text-6xl lg:text-9xl my-auto lg:pt-[128px]">Weddings</h1>
+        <section className={`flex bg-wedding-hero absolute top-0 w-full h-[400px] lg:h-[650px] bg-cover bg-center -z-10 justify-center`}>
+            <h1 className="text-center text-white font-bold text-[65px] lg:text-9xl my-auto lg:pt-[128px] pt-[100px] hero-title">Weddings</h1>
         </section>
     );
 };
