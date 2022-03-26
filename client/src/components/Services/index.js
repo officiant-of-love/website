7f2137230037aa10b837aa8878d79a552c14df39
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-26 14:13:29 -0700

    services section now responsive :)

--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -1,13 +1,14 @@
 
 const Services = ({servicesArr}) => {
   return (
-    <section className="absolute flex top-[400px] lg:top-[650px] w-full h-[850px] justify-center">
-      <div className="bg-[#F7F3EB] flex flex-col m-auto w-[85%] p-6 text-center">
-        <h1 className="font-semibold text-5xl mb-6 ">Services</h1>
-        <div className="flex mx-auto">
+    <section className="absolute flex top-[400px] lg:top-[650px] w-full justify-center py-10">
+      <div className="md:bg-[#F7F3EB] flex flex-col mx-auto md:w-[85%] text-center drop-shadow-lg p-3 md:p-6 ">
+        <div className="bg-[#F7F3EB] absolute top-0 left-[12.5%] -z-10 w-3/4 h-[1550px] md:hidden"/>
+        <h1 className="font-semibold text-5xl md:mb-6 ">Services</h1>
+        <div className="flex flex-col md:flex-row mx-auto">
           {servicesArr.map((service) => (
-            <div className="flex flex-col mx-2 lg:w-[330px]">
-              <h2 className="text-3xl">{service.name}</h2>
+            <div className="flex flex-col mx-2 my-4 md:w-1/3">
+              <h2 className="text-4xl md:text-3xl">{service.name}</h2>
               <img className="m-4" src={service.photo} alt="" />
               <p className="text-xl">{service.description}</p>
             </div>
