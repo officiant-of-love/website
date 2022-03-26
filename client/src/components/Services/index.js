637d6dff64b8407b5a2206d989d90922bd661114
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-26 10:03:59 -0700

    adjust so services div is centered, and made the services component dynamic

--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -1,50 +1,22 @@
-import ringsPhoto from './rings.jpg';
-import vowsPhoto from './vows.jpg';
-import ceremonyPhoto from './ceremony.jpg';
 
-const Services = () => {
-    const servicesArr = [
-        {
-            name: 'Simple Ceremony',
-            photo: ringsPhoto,
-            description: 'The Simple Ceremony package includes one love poem reading, reading of marriage intentions, vow exchanges, the ring exchange & the signing of marriage certificate.'
-        },
-        {
-            name: 'Custom Ceremony',
-            photo: vowsPhoto,
-            description: 'The Custom Ceremony package includes a personalized love story reading, reading of the marriage intentions, multiple love poem readings, vow exchange option readings, ring exchanges option readings, personalized acknowledgements to those most important in the couples lives, a unity candle lighting, optional additional custom requests & the signing of the marriage certificate.'
-        },
-        {
-            name: 'Ceremony + MC Host',
-            photo: ceremonyPhoto,
-            description: 'The Ceremony plus MC Host package includes all that is included in the Custom Ceremony package plus additional hours of services to MC one transition of the wedding to the next.'
-        },
+const Services = ({servicesArr}) => {
 
-    ];
-    return(
-        <section className="absolute flex top-[400px] lg:top-[650px] w-full h-[965px] justify-center">
-            <div className="bg-[#F7F3EB] flex flex-col m-auto w-[85%] p-6 text-center">
-                <h1 className="font-semibold text-5xl mb-6 ">Services</h1>
-                <div className="flex">
-                    <div className="flex flex-col mx-2 lg:w-[330px]">
-                        <h2 className='text-3xl'>{servicesArr[0].name}</h2>
-                        <img className='m-4' src={servicesArr[0].photo} alt="" />
-                        <p className="text-xl">{servicesArr[0].description}</p>
-                    </div>
-                    <div className="flex flex-col mx-2 lg:w-[330px]">
-                        <h2 className='text-3xl'>{servicesArr[1].name}</h2>
-                        <img className='m-4' src={servicesArr[1].photo} alt="" />
-                        <p className="text-xl">{servicesArr[1].description}</p>
-                    </div>
-                    <div className="flex flex-col mx-2 lg:w-[330px]">
-                        <h2 className='text-3xl'>{servicesArr[2].name}</h2>
-                        <img className='m-4' src={servicesArr[2].photo} alt="" />
-                        <p className="text-xl">{servicesArr[2].description}</p>
-                    </div>
-                </div>
+  return (
+    <section className="absolute flex top-[400px] lg:top-[650px] w-full h-[965px] justify-center">
+      <div className="bg-[#F7F3EB] flex flex-col m-auto w-[85%] p-6 text-center">
+        <h1 className="font-semibold text-5xl mb-6 ">Services</h1>
+        <div className="flex mx-auto">
+          {servicesArr.map((service) => (
+            <div className="flex flex-col mx-2 lg:w-[330px]">
+              <h2 className="text-3xl">{service.name}</h2>
+              <img className="m-4" src={service.photo} alt="" />
+              <p className="text-xl">{service.description}</p>
             </div>
-        </section>
-    );
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
--- client/src/pages/Weddings/index.js
+++ client/src/pages/Weddings/index.js
@@ -1,12 +1,35 @@
 import Hero from '../../components/Hero';
 import Services from '../../components/Services';
 
+import ringsPhoto from "./rings.jpg";
+import vowsPhoto from "./vows.jpg";
+import ceremonyPhoto from "./ceremony.jpg";
 
 const Weddings = () => {
+    const servicesArr = [
+        {
+          name: "Simple Ceremony",
+          photo: ringsPhoto,
+          description:
+            "The Simple Ceremony package includes one love poem reading, reading of marriage intentions, vow exchanges, the ring exchange & the signing of marriage certificate.",
+        },
+        {
+          name: "Custom Ceremony",
+          photo: vowsPhoto,
+          description:
+            "The Custom Ceremony package includes a personalized love story reading, reading of the marriage intentions, multiple love poem readings, vow exchange option readings, ring exchanges option readings, personalized acknowledgements to those most important in the couples lives, a unity candle lighting, optional additional custom requests & the signing of the marriage certificate.",
+        },
+        {
+          name: "Ceremony + MC Host",
+          photo: ceremonyPhoto,
+          description:
+            "The Ceremony plus MC Host package includes all that is included in the Custom Ceremony package plus additional hours of services to MC one transition of the wedding to the next.",
+        },
+      ];
     return(
         <main>
-            <Hero />
-            <Services />
+            <Hero />f
+            <Services servicesArr={servicesArr}/>
         </main>
     );
 };
