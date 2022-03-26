c2fd517ac5697e90be2bb8686d940e4955bc30df
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-26 14:23:44 -0700

    added some distance between Recs and Services

--- client/src/components/Recommendations/index.js
+++ client/src/components/Recommendations/index.js
@@ -1,7 +1,7 @@
 const Recommendations = () => {
     return(
-        <section className="bg-[#F7F3EB] absolute top-[2045px] w-full">
-            <h2 className="text-center text-5xl font-semibold">Recommendations</h2>
+        <section className="bg-[#F7F3EB] absolute top-[2075px] w-full">
+            <h2 className="text-center text-5xl font-semibold my-8">Recommendations</h2>
             <div className=""></div>
         </section>
     );
