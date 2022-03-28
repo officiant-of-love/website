f4ae70a2e3506683f37a1c3cd94100f5c941d427
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-28 12:11:58 -0700

    error resolution: added keys in Services, and class -> className; added basic clickHandlers

--- client/src/components/Recommendations/index.js
+++ client/src/components/Recommendations/index.js
@@ -4,12 +4,21 @@ import backArrow from './back-arrow.svg';
 import nextArrow from './next-arrow.svg';
 
 const Recommendations = ({recArr}) => {
-    const [rec, setRec] = useState(recArr[0])
+    const [rec, setRec] = useState(recArr[0]);
+
+    const backClickHandler = () => {
+        console.log('back');
+    };
+
+    const nextClickHandler = () => {
+        console.log('next')
+    };
+
     return(
         <section className="bg-[#F7F3EB] absolute top-[2075px] md:top-[1430px] lg:top-[1460px] w-full flex flex-col py-10">
             <h2 className="text-center text-5xl font-semibold mb-8">Recommendations</h2>
-            <div class="flex justify-around">
-                <img src={backArrow} className="drop-shadow-md h-12 my-auto" />
+            <div className="flex justify-around">
+                <img src={backArrow} className="drop-shadow-md h-12 my-auto" onClick={backClickHandler}/>
                 <div className="bg-white flex flex-row w-[65%] p-7 rounded-3xl mx-8">
                     <img src={rec.img} className="object-none object-center"/>
                     <div className='flex flex-col ml-6 text-2xl'>
@@ -17,7 +26,7 @@ const Recommendations = ({recArr}) => {
                         <p className='font-bold text-right'>- {rec.reviewer}</p>
                     </div>
                 </div>
-                <img src={nextArrow} className="drop-shadow-md h-12 my-auto"/>
+                <img src={nextArrow} className="drop-shadow-md h-12 my-auto" onClick={nextClickHandler}/>
             </div>
         </section>
     );
--- client/src/components/Services/index.js
+++ client/src/components/Services/index.js
@@ -6,8 +6,8 @@ const Services = ({servicesArr}) => {
         <div className="bg-[#F7F3EB] absolute top-0 left-[12.5%] -z-10 w-3/4 h-[1575px] drop-shadow-lg md:hidden"/>
         <h1 className="font-semibold text-5xl md:mb-6 ">Services</h1>
         <div className="flex flex-col md:flex-row mx-auto">
-          {servicesArr.map((service) => (
-            <div className="flex flex-col mx-2 my-4 md:w-1/3">
+          {servicesArr.map((service, i) => (
+            <div className="flex flex-col mx-2 my-4 md:w-1/3" key={i}>
               <h2 className="text-4xl md:text-3xl">{service.name}</h2>
               <img className="m-4" src={service.photo} alt="" />
               <p className="text-xl">{service.description}</p>
