aaaaff094cca1abf8b5ea4ab04da4279ecf1a3b4
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-03-28 12:38:40 -0700

    added functionality to the rec arrows

--- client/src/components/Recommendations/index.js
+++ client/src/components/Recommendations/index.js
@@ -4,15 +4,25 @@ import backArrow from './back-arrow.svg';
 import nextArrow from './next-arrow.svg';
 
 const Recommendations = ({recArr}) => {
-    const [rec, setRec] = useState(recArr[0]);
+    const [recNum, setRecNum] = useState(0);
 
     const backClickHandler = () => {
-        console.log('back');
+        if(recNum === 0) {
+            setRecNum(recArr.length -1);
+            return;
+        }
+        setRecNum(recNum -1);
     };
 
     const nextClickHandler = () => {
-        console.log('next')
+        if(recNum == recArr.length -1) {
+            setRecNum(0)
+            return;
+        }
+        setRecNum(recNum + 1);
     };
+    
+    const rec = recArr[recNum];
 
     return(
         <section className="bg-[#F7F3EB] absolute top-[2075px] md:top-[1430px] lg:top-[1460px] w-full flex flex-col py-10">
@@ -20,9 +30,9 @@ const Recommendations = ({recArr}) => {
             <div className="flex justify-around">
                 <img src={backArrow} className="drop-shadow-md h-12 my-auto" onClick={backClickHandler}/>
                 <div className="bg-white flex flex-row w-[65%] p-7 rounded-3xl mx-8">
-                    <img src={rec.img} className="object-none object-center"/>
+                    <img src={rec.img} className="object-none object-center max-w-md"/>
                     <div className='flex flex-col ml-6 text-2xl'>
-                        <p className='leading-none'>{rec.text}</p>
+                        <p className='leading-none'>"{rec.text}"</p>
                         <p className='font-bold text-right'>- {rec.reviewer}</p>
                     </div>
                 </div>
