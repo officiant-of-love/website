d7e7320d1b708edd4bfc28bb8b7498914d2b5f7b
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-08 14:36:18 -0700

    small styling changes for responsiveness

--- client/src/components/Recommendations/index.js
+++ client/src/components/Recommendations/index.js
@@ -36,18 +36,18 @@ const Recommendations = ({recArr}) => {
     };
 
     const zoomHandler = ({ reviewer }) => {
-        return reviewer === 'Holly Bluhm' ? `lg:max-w-md` : `object-cover lg:object-center lg:max-w-md` 
+        return reviewer === 'Holly Bluhm' ? `` : `object-cover lg:w-1/2` 
     };
 
     return(
         <section className="bg-[#F7F3EB] absolute top-[2075px] md:top-[1430px] lg:top-[1460px] w-full flex flex-col py-10" id="recommendations">
             <h2 className="text-center text-5xl font-semibold mb-8">Recommendations</h2>
-            <div className="flex flex-col lg:flex-row justify-center">
+            <div className="flex flex-col lg:flex-row lg:justify-evenly lg:min-h-[640px]">
                 <img src={fullBackArrow} className="drop-shadow-md h-12 my-auto hidden lg:flex" onClick={backClickHandler}/>
-                <div className="bg-white flex flex-col lg:flex-row w-[95%] lg:w-[65%] p-7 rounded-3xl mx-auto">
-                    <img src={rec.img} className={zoomHandler(rec)}/>
-                    <div className='flex flex-col lg:ml-6 text-2xl pt-3 lg:pt-0 my-auto'>
-                        <p className='lg:leading-none text-xl text-left'>"{rec.text}"</p>
+                <div className="bg-white flex flex-col lg:flex-row w-[95%] md:w-[65%] mx-auto lg:mx-0 p-7 rounded-3xl justify-center">
+                    <img src={rec.img} className='object-cover lg:w-1/2'/>
+                    <div className='flex flex-col lg:ml-6 text-2xl pt-3 lg:pt-0 my-auto lg:w-1/2'>
+                        <p className='lg:leading-none text-left'>"{rec.text}"</p>
                         <p className='font-bold text-right overflow-scroll'>- {rec.reviewer}</p>
                     </div>
                 </div>
