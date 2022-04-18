6a48a15eac8934834e35d729c66e082f5b479f7d
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-18 10:30:09 -0700

    add basic Intro section

--- client/src/pages/About/index.js
+++ client/src/pages/About/index.js
@@ -1,9 +1,11 @@
 import Hero from '../../components/Hero';
+import Intro from '../../components/Introduction';
 
 const About = () => {
     return (
         <>
             <Hero page="about" />
+            <Intro />
         </>
     );
 };
