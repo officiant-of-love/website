450338306199b10d457c8040e4d44b6150dc89de
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-20 14:43:29 -0700

    basic form functionality

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,19 +1,44 @@
+import { useState } from 'react';
+
 const Form = () => {
+    const [formValues, setFormValues] = useState({
+        name: '',
+        phone: '',
+        service: '',
+        message: ''
+    });
+    
+    const changeHandler = (e) => {
+        let { name, value } = e.target;
+        if(name === 'phone') {
+            const validatedValue = value.replace(/[^0-9]/g, "");
+            value = validatedValue;
+        }
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    };
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log(formValues)
+    };
     return (
         <form className="w-full flex flex-col">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" className="bg-[#F7F3EB] w-full border border-black/20"/>
+            <input type="text" name="name" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
             <label htmlFor="phone">Phone</label>
-            <input type="text" name="phone" className="bg-[#F7F3EB] w-full border border-black/20"/>
+            <input type="tel" name="phone" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
             <label htmlFor="service">Service</label>
-            <select name="service" className="bg-[#F7F3EB] w-full border border-black/20 ">
+            <select name="service" className="bg-[#F7F3EB] w-full border border-black/20 " onChange={changeHandler}>
                 <option value="" className="text-black/20">Select a service</option>
                 <option value="wedding">Wedding</option>
                 <option value="funeral">Funeral</option>
             </select>
             <label htmlFor="message">Message</label>
-            <textarea type="text" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20"/>
-            <button type="submit" className="bg-[#F7F3EB] w-1/4 mx-auto border border-black/20 mt-5">Submit</button>
+            <textarea type="text" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
+            <button type="button" className="bg-[#F7F3EB] w-1/4 mx-auto border border-black/20 mt-5" onClick={submitHandler}>Submit</button>
         </form>
     );
 };
