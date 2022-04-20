0cb9b4fd515f2a0c76fda66892a3fdf475659723
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-20 15:29:11 -0700

    correct label attribution in form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -27,17 +27,17 @@ const Form = () => {
     return (
         <form className="w-full flex flex-col">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
+            <input type="text" name="name" id="name" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
             <label htmlFor="phone">Phone</label>
-            <input type="tel" name="phone" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
+            <input type="tel" name="phone" id="phone" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
             <label htmlFor="service">Service</label>
-            <select name="service" className="bg-[#F7F3EB] w-full border border-black/20 " onChange={changeHandler}>
+            <select name="service" id="service" className="bg-[#F7F3EB] w-full border border-black/20 " onChange={changeHandler}>
                 <option value="" className="text-black/20">Select a service</option>
                 <option value="wedding">Wedding</option>
                 <option value="funeral">Funeral</option>
             </select>
             <label htmlFor="message">Message</label>
-            <textarea type="text" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
+            <textarea type="text"  id="message" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
             <button type="button" className="bg-[#F7F3EB] w-1/4 mx-auto border border-black/20 mt-5" onClick={submitHandler}>Submit</button>
         </form>
     );
