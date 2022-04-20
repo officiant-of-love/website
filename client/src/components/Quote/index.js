c248d6abc059005708e685a24bb5ce9bcbb39370
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-20 13:41:47 -0700

    add Quote component to funerals page

---
+++ client/src/components/Quote/index.js
@@ -0,0 +1,11 @@
+const Quote = () => {
+    return(
+        <section className="px-5 my-6">
+            <h2 className="text-6xl py-4">"Everything has a beginning and an end</h2>
+            <h2 className="text-6xl text-right font-bold py-4">Life is just a cycle of starts and stops</h2>
+            <h2 className="text-6xl text-center py-4 w-[85%] mx-auto">There are ends we don't desire but they're inevitable, we have to face them. It's what being human is all about" - Jet Black</h2>
+        </section>
+    );
+};
+
+export default Quote;
--- client/src/pages/Funerals/index.js
+++ client/src/pages/Funerals/index.js
@@ -1,10 +1,11 @@
 import Hero from "../../components/Hero";
+import Quote from '../../components/Quote';
 
 const Funerals = () => {
     return (
         <>
             <Hero page="funeral"/>
-            
+            <Quote />
         </>
     );
 };
