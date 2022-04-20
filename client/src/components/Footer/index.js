ae76f2aa998ad7290ebcb2b5a1a114f183663ec5
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-20 13:31:48 -0700

    update footer year

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,7 @@
 const Footer = () => {
 
     return (
-        <footer className="text-center py-5 text-4xl">Officiant of Love est. 2015</footer>
+        <footer className="text-center py-5 text-4xl">Officiant of Love est. 2018</footer>
     );
 };
 
