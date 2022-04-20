e21455b5f11892402c77ebcf28354df90a630970
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-20 13:53:01 -0700

    add candlephoto for funerals page

--- client/src/pages/Funerals/index.js
+++ client/src/pages/Funerals/index.js
@@ -1,11 +1,14 @@
 import Hero from "../../components/Hero";
 import Quote from '../../components/Quote';
 
+import candlePhoto from './assets/candles.jpg';
+
 const Funerals = () => {
     return (
         <>
             <Hero page="funeral"/>
             <Quote />
+
         </>
     );
 };
