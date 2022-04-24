284ea2f3e8760bde0234f992fcf61355ff5cdf84
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-04-24 14:27:40 -0700

    added state related form text confirming when Form submitted

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+    const [sent, setSent] = useState(false);
     const [formValues, setFormValues] = useState({
         name: '',
         phone: '',
         service: '',
         message: ''
     });
-    
+
     const changeHandler = (e) => {
         let { name, value } = e.target;
-        if(name === 'phone') {
+        if (name === 'phone') {
             const validatedValue = value.replace(/[^0-9]/g, "");
             value = validatedValue;
         }
@@ -22,23 +24,37 @@ const Form = () => {
 
     const submitHandler = (e) => {
         e.preventDefault();
-        console.log(formValues)
+        emailjs.send('service_kt8il4a', 'template_rvwxk9q', formValues, 'Y4DVl1ny8VVvZKICh')
+            .then((result) => {
+                setSent(true);
+                console.log("yay!",result.text);
+            }, (error) => {
+                console.log("oh no :(",error.text);
+            });
     };
     return (
         <form className="w-full flex flex-col">
-            <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
-            <label htmlFor="phone">Phone</label>
-            <input type="tel" name="phone" id="phone" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
-            <label htmlFor="service">Service</label>
-            <select name="service" id="service" className="bg-[#F7F3EB] w-full border border-black/20 " onChange={changeHandler}>
-                <option value="" className="text-black/20">Select a service</option>
-                <option value="wedding">Wedding</option>
-                <option value="funeral">Funeral</option>
-            </select>
-            <label htmlFor="message">Message</label>
-            <textarea type="text"  id="message" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler}/>
-            <button type="button" className="bg-[#F7F3EB] w-1/4 mx-auto border border-black/20 mt-5" onClick={submitHandler}>Submit</button>
+           {sent ? (
+               <h2 className='text-center'>
+                   Form successfully submitted!
+               </h2>
+           ) : (
+               <>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler} />
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" name="phone" id="phone" className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler} />
+                <label htmlFor="service">Service</label>
+                <select name="service" id="service" className="bg-[#F7F3EB] w-full border border-black/20 " onChange={changeHandler}>
+                    <option value="" className="text-black/20">Select a service</option>
+                    <option value="wedding">Wedding</option>
+                    <option value="funeral">Funeral</option>
+                </select>
+                <label htmlFor="message">Message</label>
+                <textarea type="text" id="message" name="message" rows='5' className="bg-[#F7F3EB] w-full border border-black/20" onChange={changeHandler} />
+                <button type="button" className="bg-[#F7F3EB] w-1/4 mx-auto border border-black/20 mt-5" onClick={submitHandler}>Submit</button>
+               </>
+           )}
         </form>
     );
 };
