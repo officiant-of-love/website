7e2fb3dc9fd29d82cb74e498c2330508f10786c6
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-05-15 12:43:34 -0700

    fixed mobile issue I think???

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -3,7 +3,7 @@ const Services = ({servicesArr}) => {
   return (
     <section className="flex w-full justify-center py-10">
       <div className="md:bg-[#F7F3EB] flex flex-col mx-auto md:w-[85%] text-center drop-shadow-lg p-3 md:p-6 ">
-        <div className="bg-[#F7F3EB] absolute top-0 left-[12.5%] -z-10 w-3/4 h-[1575px] drop-shadow-lg md:hidden"/>
+        <div className="bg-[#F7F3EB] absolute left-[12.5%] -z-10 w-3/4 h-[1575px] drop-shadow-lg md:hidden"/>
         <h1 className="font-semibold text-5xl md:mb-6 ">Services</h1>
         <div className="flex flex-col md:flex-row mx-auto">
           {servicesArr.map((service, i) => (
