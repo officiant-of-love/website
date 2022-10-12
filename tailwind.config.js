c10a7f0cc78b2ae1f4f03ae7461513ee1f072d24
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-10-12 16:29:33 -0700

    change about image

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         'wedding-hero': 'url(./pages/Weddings/assets/wedding-flowers.jpg)',
-        'about-hero': 'url(./pages/About/assets/LA-skyline.jpg)',
+        'about-hero': 'url(./pages/About/assets/About.jpg)',
         'funeral-hero': 'url(./pages/Funerals/assets/funerals.jpg)',
         'contact-hero': 'url(./pages/Contact/assets/book.jpg)'
       }
