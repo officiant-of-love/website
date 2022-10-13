bb6cd13b96f68f1f9e98e3c0fb527a6b39c85f28
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-10-12 17:13:20 -0700

    change quote

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -1,9 +1,7 @@
 const Quote = () => {
     return(
         <section className="px-5 my-6">
-            <h2 className="text-4xl md:text-6xl text-center md:text-left py-4">"Everything has a beginning and an end.</h2>
-            <h2 className="text-4xl md:text-6xl text-center md:text-right font-bold py-4">Life is just a cycle of starts and stops.</h2>
-            <h2 className="text-4xl md:text-6xl text-center py-4 md:w-[85%] mx-auto">There are ends we don't desire but they're inevitable. We have to face them. It's what being human is all about."<br className="md:hidden"/> - Jet Black</h2>
+            <h2 className="text-4xl md:text-6xl text-center py-4">“The connections we make in the course of a life – maybe that’s what heaven is” – Fred Rogers</h2>
         </section>
     );
 };
